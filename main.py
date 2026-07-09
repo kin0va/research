867d95f1de2ec0d2afe2613f902deb05affb50ce
kin0va <eoinkinsellamusic@gmail.com>
2026-07-09 19:43:40 +0100

    ending weork for the day, just need to wire up the graphing module tomorrow and were in business

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-
-import importlib
-
+from cli.parseargs import parse_args
 from rich.console import Console
-
+from apis.csvintake import read_monitor_output_from_csv
+from apis.jsonintake import single_room_json
+from classes.room_params import RoomParams
+from classes.monitor_output import MonitorOutput
+from classes.sitedata import SiteData
+from approaches.kalman import apply_kalman_filter
+from pathlib import Path
 
 def main() -> int:
     console = Console()
-    cli_parser = importlib.import_module("cli.parse-args")
-    args = cli_parser.parse_args()
+    args = parse_args()
 
-    # Print guide if requested
     if args.guide:
-        cli_parser.print_guide(console)
-        return 0
+        from cli.parseargs import print_guide
 
-    console.print("[bold red]Error:[/bold red] No functionality implemented yet.")
+        print_guide(console)
+        return 0
+    # Run the main program
+    # Implementingthe kalman filter approach to estimate occupancy and ACH
+    room_json = args.room_json or Path("data/json/room.json")
+    monitor_csv = args.monitor_csv or Path("data/csv/monitor.csv")
+    room_params = single_room_json(room_json) #currently only one room supported while setting up
+    monitor_output = read_monitor_output_from_csv(monitor_csv)
+    site_data = SiteData(site_name=args.site_name or "Default Site", room_params=room_params[1], monitor_output=monitor_output)
+    plotting_data = apply_kalman_filter(site_data, room_params[1])
     return 1
 
 
